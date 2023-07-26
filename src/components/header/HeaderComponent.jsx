e2a49c46213d4dd621ab2d3c2dc35b5d4388ecda
cristhianbrh <cristhianalexand@gmail.com>
2023-07-25 21:58:34 -0500

    Header responsive good

--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -3,9 +3,10 @@ import myIcon from './../../sources/img/logo/logo.ico'
 import React, { useContext, useId, useState } from "react";
 import { LanguageF } from "../../hooks/ContextLanguage";
 
-const HeaderComponent = ({optionAct, locationSection, sections}) => {
-    const {language, setLanguage} = useContext(LanguageF);
+const HeaderComponent = ({ optionAct, locationSection, sections }) => {
+    const { setLanguage } = useContext(LanguageF);
     const [lanAct, setLanAct] = useState(true);
+    const [menuActive, setMenuActive] = useState(false);
 
     const styleOption = [{
         color: "#aba6a6",
@@ -25,19 +26,19 @@ const HeaderComponent = ({optionAct, locationSection, sections}) => {
     //         }
     //     }))
     // });
-    
-    const isPressedLenguage = ()=>{
+
+    const isPressedLenguage = () => {
         //true:en
         //false:es
-        
+
         //false
-        setLanguage(lanAct?"es":"en");
+        setLanguage(lanAct ? "es" : "en");
 
         setLanAct(!lanAct);
         //false
-        console.log(language)
-    } 
-    
+
+    }
+
 
     return (
         <header className="HeaderComponent-class">
@@ -45,24 +46,30 @@ const HeaderComponent = ({optionAct, locationSection, sections}) => {
                 <div className="HeaderComponent-class-Logo-Switch">
                     <img src={myIcon} alt="Lol" />
                     <label>
-                        <input type="checkbox" className="HeaderComponent-class-Logo-Switch-input" onClick={isPressedLenguage}/>
+                        <input type="checkbox" className="HeaderComponent-class-Logo-Switch-input" onClick={isPressedLenguage} />
                         <span className="HeaderComponent-class-Logo-Switch-span">es en</span>
                     </label>
                 </div>
 
                 <div className="HeaderComponent-class-sections">
-                    {sections.map((section, num) => {
-                        
-                        return(<React.Fragment key={"HeaderComponent-class_"+num}>
-                            <a key={"HeaderComponent_sections1_"+num} onClick={()=>locationSection(num)}
-                                style={(optionAct===num)?styleOption[1]:styleOption[0]}>{section}</a>
-                            {(num + 1 < sections.length) ? (<span key={"HeaderComponent_sections1_span_"+num}>|</span>) : null}
-                        </React.Fragment>)}
-                    )}
+                    <svg xmlns="http://www.w3.org/2000/svg" onClick={()=>setMenuActive(!menuActive)} width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                    </svg>
+                    <div className={!menuActive?"HeaderComponent-class-sectionsDiv__Noactive":""}>
+                        {sections.map((section, num) => {
+
+                            return (<React.Fragment key={"HeaderComponent-class_" + num}>
+                                <a key={"HeaderComponent_sections1_" + num} onClick={() => locationSection(num)}
+                                    style={(optionAct === num) ? styleOption[1] : styleOption[0]}>{section}</a>
+                                {(num + 1 < sections.length) ? (<span key={"HeaderComponent_sections1_span_" + num}>|</span>) : null}
+                            </React.Fragment>)
+                        }
+                        )}
+                    </div>
                 </div>
             </nav>
         </header>
     )
-    
+
 }
 export default HeaderComponent;
